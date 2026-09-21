90812c55ddcba028dab2d5f3f4a2723ddec18e7c
hassanaly18 <hassansiddiqui03024053707@gmail.com>
2026-09-21 19:47:38 +0500

    Prompts in LangChain, static vs dynamic, template, chatprompttemplate, Messages, messagesPlaceholder

---
+++ LangChainPrompts/messages.py
@@ -0,0 +1,22 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    provider="auto",
+   # temperature=1
+)
+
+model = ChatHuggingFace(llm=llm)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content="Tell me about LangChain")
+    ]
+
+result = model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+
+print(messages)
