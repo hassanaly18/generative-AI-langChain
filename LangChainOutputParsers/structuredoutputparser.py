a5a869cabb41d6983504d483bfd5f807c8de1cf1
hassanaly18 <hassansiddiqui03024053707@gmail.com>
2026-09-22 20:00:07 +0500

    Structured output parser

---
+++ LangChainOutputParsers/structuredoutputparser.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.output_parsers.structured import StructuredOutputParser, ResponseSchema
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    provider="auto"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name="fact_1", description="Fact 1 about thetopic"),
+    ResponseSchema(name="fact_2", description="Fact 2 about thetopic"),
+    ResponseSchema(name="fact_3", description="Fact 3 about thetopic")
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template="Give 3 facts about {topic} \n {format_instruction}",
+    input_variables=["topic"],
+    partial_variables={"format_instruction": parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+# prompt = template.invoke({"topic": "Black Hole"})
+result = chain.invoke({"topic": "Black Hole"})
+
+# final_result = parser.parse(result.content)
+print(result)
